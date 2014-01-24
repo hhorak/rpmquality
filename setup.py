adf73798d0a09d071fcea2b914137e8a6c0bb810
Honza Horak <horak.honza@gmail.com>
2014-01-24 07:54:33 +0100

    Added some new files

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup, find_packages
+
+setup(
+    name = 'rpmquality',
+    version = '0.1',
+    description = 'Short description',
+    long_description = 'Long description',
+    keywords = 'some, keywords',
+    author = 'Honza Horak',
+    author_email = 'hhorak@redhat.com',
+    license = 'BSD',
+    packages = find_packages(),
+    entry_points={'console_scripts':['rpmquality = rpmquality.bin:main']},
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Build Tools',
+                   'Topic :: System :: Software Distribution',
+                  ]
+)
